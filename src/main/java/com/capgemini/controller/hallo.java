e3f00d1d1381a3b401c4430d9124bea331493927
Seff70 <sefa.kucukdemirci@capgemini.com>
2017-05-02 16:38:45 +0200

    Spring toegevoegd

---
+++ src/main/java/com/capgemini/controller/hallo.java
@@ -0,0 +1,13 @@
+package com.capgemini.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hallo {
+
+    @RequestMapping("/hallo")
+    public String Homes(){return
+            ("Hello world");}
+
+}
