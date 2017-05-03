944b061cf2b6ba303f18537fc4e38488231c9373
Seff70 <sefa.kucukdemirci@capgemini.com>
2017-05-03 09:08:42 +0200

    changes

--- src/main/java/com/capgemini/controller/hallo.java
+++ src/main/java/com/capgemini/controller/hallo.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class hallo {
+public class Hallo {
 
     @RequestMapping("/hallo")
     public String Homes(){return
