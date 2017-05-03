6701ae92fcc3b7dd6573c797bd588c7dd3c5d8a0
MimiManna <laxmi.manna@capgemini.com>
2017-05-03 12:22:23 +0200

    Adding POSTS/GETS

---
+++ src/main/java/com/capgemini/controller/GuestController.java
@@ -0,0 +1,25 @@
+package com.capgemini.controller;
+import com.capgemini.Model.Guest;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+// het is een referentie naar een stukje code in dit geval de restcontroller
+
+public class GuestController {
+
+    @RequestMapping(value = "/guest", method= RequestMethod.GET )
+    public Guest getGuest(@RequestParam String name,@RequestParam int age){
+        // je krijgt geen data binnen maar kunt het wel uitlezen
+        // Spring maakt er automatisch js van
+        Guest g = new Guest();
+        g.setName(name);
+        g.setAge(age);
+        return g;
+    }
+    @RequestMapping(value= "/guest", method= RequestMethod.POST)
+    public String setGuest(@RequestBody Guest g){
+        return g.getName() +',' + g.getAge();
+
+    }
+
+}
--- src/main/java/com/capgemini/controller/hallo.java
+++ src/main/java/com/capgemini/controller/hallo.java
@@ -1,12 +1,13 @@
 package com.capgemini.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hallo {
 
-    @RequestMapping("/html")
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String homes(){return
             ("Hello world");}
 
