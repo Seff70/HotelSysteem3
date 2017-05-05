aa1d94d530b421efdcb98d83d95cf290300ea211
ericterheijden <eric.ter.heijden@capgemini.com>
2017-05-05 15:27:20 +0200

    Start guest
    basic field shows etc

---
+++ src/main/resources/public/js/guest.js
@@ -0,0 +1,27 @@
+$.get("api/guestlist", function(result) {
+    console.table(result);
+
+    var dataSet = [];
+
+    for (var i = 0; i<result.length; i++) {
+    console.log(result);
+    console.log(result[0].name);
+        dataSet.push({result[i].name, result[i].address, result[i].zipcode, result[i].city, result[i].country, result[i].phonenumber, result[i].special});
+    }
+
+$("#guestlist").DataTable( {
+        data: dataSet,
+
+//        columns: [
+//        {title: "Name"},
+//        {title: "Address"},
+//        {title: "Zipcode"},
+//        {title: "City"},
+//        {title: "Country"},
+//        {title: "Phonenumber"},
+//        {title: "Special"}
+        ]
+    })
+
+});
+
