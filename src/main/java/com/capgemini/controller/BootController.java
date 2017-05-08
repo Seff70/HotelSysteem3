20fc2b6125939e3246da125f68d2f979b73e6262
Seff70 <sefa.kucukdemirci@capgemini.com>
2017-05-08 16:29:32 +0200

    Botenkant is klaar !
    Eric & Sefa is klaar met BOOT KANT
    Op 9-5-2017 gaan we verder !

--- src/main/java/com/capgemini/controller/BootController.java
+++ src/main/java/com/capgemini/controller/BootController.java
@@ -1,20 +1,34 @@
 package com.capgemini.controller;
 import com.capgemini.Model.Boten.Boot;
+import com.capgemini.Model.Guests.Guest;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
+import static java.sql.DriverManager.getConnection;
 
 
 @RestController
-public class BootController {
+public class BootController extends AbstractDatabaseController {
 
     @RequestMapping(value = "/api/boot", method= RequestMethod.GET )
-    public ArrayList<Boot> boot (){
+    public ArrayList<Boot> boot() throws SQLException{
+
+        Connection connection = getConnection("hotel2");
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM Boat");
+        ResultSet rs = statement.executeQuery();
+
         ArrayList<Boot> botenList = new ArrayList<>();
-        for (int i = 0; i < 8 ; i++) {
-            Boot boot = new Boot(i,"Naam van de boot" +i);
+        while(rs.next()) {
+            int Boatnumber = rs.getInt( "Boatnumber" );
+            Boot boot = new Boot();
+            boot.setNummer(Boatnumber);
             botenList.add(boot);
+
         }
         return botenList ;
 
@@ -22,3 +36,6 @@ public class BootController {
 
 
 }
+
+
+
