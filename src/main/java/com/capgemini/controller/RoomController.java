b63b2a321cdd617d06d37cb4d65bf2f3bb69f877
MimiManna <laxmi.manna@capgemini.com>
2017-05-08 16:33:18 +0200

    Added database content to JAVA

--- src/main/java/com/capgemini/controller/RoomController.java
+++ src/main/java/com/capgemini/controller/RoomController.java
@@ -5,32 +5,44 @@ import com.capgemini.Model.Kamers.Room;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import sun.security.krb5.internal.crypto.EType;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import java.util.Random;
+
+import static java.sql.DriverManager.getConnection;
 
 /**
  * Created by LMANNA on 3-5-2017.
  */
 
 @RestController
-public class RoomController {
+public class RoomController extends AbstractDatabaseController {
 
     @RequestMapping(value = "api/kamergegevens", method = RequestMethod.GET)
-    public ArrayList<Room> get() {
-        ArrayList<Room> roomList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+    public ArrayList <Room> get() throws SQLException {
+        Connection connection = getConnection("hotel2");
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM Room");
+        ResultSet rs = statement.executeQuery();
+
+        ArrayList <Room> roomList = new ArrayList <>();
+
+        while (rs.next()) {
+            int roomNumber = rs.getInt("roomNumber");
+            String typeOfRoom = rs.getString("type");
+            Etype roomType = Etype.valueOf(typeOfRoom);
             Room room = new Room();
-            room.setRoomNumber(i);
-            room.setGuestLastName("Pietje" + i);
-            if (i < 5) {
-                room.setRoomType(Etype.Standaard);
-            } else {
-                room.setRoomType(Etype.Luxe);
-            }
+            room.setRoomNumber(roomNumber);
+            room.setRoomType(roomType);
             roomList.add(room);
         }
+
         return roomList;
     }
+
 }
