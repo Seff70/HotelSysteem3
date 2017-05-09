70373113ad2831ef08d82348fc698a2196f9b175
MimiManna <laxmi.manna@capgemini.com>
2017-05-09 14:53:27 +0200

    Edited EType, played with code on room.js

--- src/main/resources/public/js/room.js
+++ src/main/resources/public/js/room.js
@@ -8,3 +8,35 @@ $.get("/api/roomList", function(result) {
         data: roomTable
     });
 });
+$.get("api/pages", function(result) {
+
+$("#title").text(result.title);
+$("#content").text(result.content);
+
+});
+
+
+$("#btn1").click(function(event){
+    event.preventDefault();
+    var k ={kamernummer: $("#tf1").val()};
+    console.log("waarde = "+ $("#tf1").val())
+
+    $.ajax({
+
+            contentType: "application/json",
+            type: "POST",
+            url:"/api/kamernummer",
+            data: JSON.stringify(k),
+            success: function(result) {
+                        console.log(result);
+                        var s = ("Kamer " + result.kamernummer + " is een " + result.type + " kamer "   );
+                        $("#content").text(s);
+                        },
+            error: function(e){
+                  console.log(e);
+
+                  var e = ("Dit is geen geldig kamernummer!")
+                    $("#content").text(e);
+            }
+          });
+});
