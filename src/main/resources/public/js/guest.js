bd9764fb235012cd878caa6946e01b6b2e502d53
dwvvz <diederick.veldhuijzen.van.zanten@capgemini.com>
2017-05-09 16:00:37 +0200

    bookingcontroller en guest
    fix

--- src/main/resources/public/js/guest.js
+++ src/main/resources/public/js/guest.js
@@ -1,28 +1,26 @@
 $.get("api/guests", function(result) {
-    console.table(result);
+    //console.table(result);
 
     var dataSet = [];
 
     for (var i = 0; i<result.length; i++) {
-    console.log(result);
-    console.log(result[0].name);
+    //console.log(result);
+    //console.log(result[0].name);
     dataSet.push([result[i].name, result[i].address, result[i].zipcode, result[i].city, result[i].country, result[i].phonenumber, result[i].special]);
     }
 
 $("#guestlist").DataTable( {
         data: dataSet
-
-//        columns: [
-//        {title: "Name"},
-//        {title: "Address"},
-//        {title: "Zipcode"},
-//        {title: "City"},
-//        {title: "Country"},
-//        {title: "Phonenumber"},
-//        {title: "Special"}
-//        ]
     });
 
+$(document).ready(function() {
+    console.log("komt hier")
+    var table = $('#example').DataTable();
 
+    $('#example tbody').on('click', 'tr', function () {
+        var data = table.row( this ).data();
+        alert( 'You clicked on '+data[0]+'\'s row' );
+    } );
+} );
 });
 
