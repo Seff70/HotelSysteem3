c7a9d1d36d9bc3fe0442af77ce7106e5cde36de4
SandraKunnen <sandra.kunnen@capgemini.com>
2017-05-10 14:05:06 +0200

    Main adjusted

--- src/main/java/com/capgemini/Main.java
+++ src/main/java/com/capgemini/Main.java
@@ -3,9 +3,16 @@ package com.capgemini;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+//        SpringApplication.run(Main.class, args);
+        Properties properties = new Properties();
+        properties.setProperty("spring.jpa.generate-ddl","true");
+        SpringApplication springApplication = new SpringApplication(Main.class);
+        springApplication.setDefaultProperties(properties);
+        springApplication.run(args);
     }
 }
