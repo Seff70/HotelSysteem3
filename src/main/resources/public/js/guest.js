ab4cbaec00e931f2a09a3f0fc09b75a30a84029e
dwvvz <diederick.veldhuijzen.van.zanten@capgemini.com>
2017-05-10 13:34:18 +0200

    guest updated
    adsf

--- src/main/resources/public/js/guest.js
+++ src/main/resources/public/js/guest.js
@@ -8,32 +8,40 @@ $.get("api/guests", function(result) {
     //console.log(result[0].name);
     $("#newguestform").hide();
     $("#guestlist").show();
+    $("#btn2").hide();
+    $("#btn3").hide();
+
     dataSet.push([result[i].name, result[i].address, result[i].zipcode, result[i].city, result[i].country, result[i].phonenumber, result[i].special]);
     }
 
-$("#guestlist").DataTable( {
-        data: dataSet
+    $("#guestlist").DataTable( {
+            data: dataSet
+        });
+
+    $(document).ready(function() {
+        console.log("komt hier")
+        var table = $('#guestlist').DataTable();
+
+        $('#guestlist tbody').on('click', 'tr', function () {
+            var data = table.row( this ).data();
+            console.log(data);
+            $("#newguestform").show();
+            $("#guestlist").hide();
+            $("#btn1").hide();
+            $("#btn2").show();
+            $("#btn3").show();
+
+            $("#InputName").val(data[0]);
+            $("#InputAddress").val(data[1]);
+            $("#InputZipcode").val(data[2]);
+            $("#InputCity").val(data[3]);
+            $("#InputCountry").val(data[4]);
+            $("#InputTelephone").val(data[5]);
+            $("#InputSpecial").val(data[6]);
+    //        alert( 'You clicked on '+data[0]+'\'s row' );
+        });
     });
 
-$(document).ready(function() {
-    console.log("komt hier")
-    var table = $('#guestlist').DataTable();
 
-    $('#guestlist tbody').on('click', 'tr', function () {
-        var data = table.row( this ).data();
-        console.log(data);
-        $("#newguestform").show();
-        $("#guestlist").hide();
-
-        $("#InputName").val(data[0]);
-        $("#InputAddress").value(data[1]);
-        $("#InputZipcode").val(data[2]);
-        $("#InputCity").val(data[3]);
-        $("#InputCountry").val(data[4]);
-        $("#InputTelephone").val(data[5]);
-        $("#InputSpecial").val(data[6]);
-//        alert( 'You clicked on '+data[0]+'\'s row' );
-    } );
-} );
 });
 
