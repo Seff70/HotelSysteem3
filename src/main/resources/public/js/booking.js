af8f5ec8278b827b3f6004bc25b38f4488926851
dwvvz <diederick.veldhuijzen.van.zanten@capgemini.com>
2017-05-12 16:00:08 +0200

    booking edts

--- src/main/resources/public/js/booking.js
+++ src/main/resources/public/js/booking.js
@@ -1,22 +1,9 @@
 $.get("api/bookings", function(result) {
+    console.log("start van booking.js, get all bookings: ");
     console.table(result);
 
     //zodat we stap 1 en twee overslaan, en direct naar gast-kiezen gaan.
     toGuestSelect();
-//    var dataSet = [];
-//
-//    for (var i = 0; i<result.length; i++) {
-//     console.log("result: "+  result);
-//     console.log("result[0]: " + result[0] + " "+ result[0].guest.name);
-//     var start = "" + result[i].start[2]+ "-" + result[i].start[1] + "-" + result[i].start[0]
-//     var eind = "" + result[i].eind[2]+ "-" + result[i].eind[1] + "-" + result[i].eind[0]
-//
-//     dataSet.push([result[i].bookingNumber, result[i].guest.name, result[i].guest.address, start, eind, result[i].room.roomNumber, result[i].room.roomType]);
-//    }
-//
-//    $("#bookings").DataTable( {
-//            data: dataSet
-//        });
 
     $("#availableRooms tbody").on('click', 'tr', function () {
         event.preventDefault();
@@ -25,10 +12,11 @@ $.get("api/bookings", function(result) {
     });
 
 });
-//to room select
+
 function toDateSelect () {
         $("#datetimepicker1").show();
         $("#datetimepicker2").show();
+
 }
 
 function toRoomSelect() {
@@ -36,8 +24,6 @@ function toRoomSelect() {
 }
 
 function toGuestSelect() {
-    $("#GuestPickerTable").show();
-
     $.get("/api/guests",function (result){
         console.table(result);
         var table = $('#GuestPickerTable').DataTable({
@@ -55,16 +41,47 @@ function toGuestSelect() {
         });
     });
 
+    $("#GuestPickerTable").show();
+
+    console.log("hiero");
+
     $("#GuestPickerTable tbody").on('click', 'tr', function () {
            event.preventDefault();
            console.log("tot hier")
            var table = $("#GuestPickerTable").DataTable();
            var guest = table.row( this ).data();
            table.search(guest.name).draw();
+           toConfirmBooking();
     });
 }
 
 function toConfirmBooking () {
     $("#ConfirmBooking").show();
+    $("#confirmBookingButton").click(function(
+
+        var guestTable = $("#GuestPickerTable").DataTable());
+        var roomTable = $("availableRooms").DataTable());
+        var newBooking = {
+            start   : $("#start").val(),
+            end     : $("#end").val(),
+            guest   : guestTable.row(0).data();
+            room    : roomTable.row(0).data();
+        };
+        }
+        $.ajax({
+            contentType: "application/json",
+            type: "POST",
+            url:"/api/addbooking",
+            data: JSON.stringify(newBooking),
+            success: function(result) {
+                        console.log(result);
+                        toDateSelect();
+            },
+            error: function(e){
+                  console.log(e);
+                  var e = ("Het is niet gelukt om een boeking toe te voegen")
+            }
+        });
+    );
 }
 
