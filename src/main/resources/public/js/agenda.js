b7bb1d63a99a819ac6aadf7c0e59fdd41a9b04f9
Seff70 <Askimsensin1!>
2017-05-12 16:23:40 +0200

    auto default startdate ingesteld bij bookingen. Sandra & Sefa

--- src/main/resources/public/js/agenda.js
+++ src/main/resources/public/js/agenda.js
@@ -11,29 +11,18 @@ $.get("api/bookings", function (result) {
     console.table(result);
     var table = $('#availableRooms').DataTable({
         columns: [
-            {data: "bookingNumber"},
             {data: "room.roomType"},
-            {data: "guest.name"},
-            {data: "start"},
-            {data: "eind"}
-
+            {data: "room.roomNumber"},
         ],
         data: result
     });
 
     });
 
- $.ajax({
-        contentType: "application/json",
-        type: "get",
-        url: "api/bookings",
-        success: function (result) {
-            console.log(result);
 
-               },
-        error: function (e) {
-            console.log(e);
-            $("#confirmaddroom").text("Uw invoer is niet correct.");
-        }
+$("#submitagenda").click(function(event){
+            var input
+            if(input < start)
 
-    });
+            $('#availableRooms').show();
+            });
--- src/main/resources/public/js/booking.js
+++ src/main/resources/public/js/booking.js
@@ -8,8 +8,15 @@ $.get("api/bookings", function(result) {
     for (var i = 0; i<result.length; i++) {
      console.log("result: "+  result);
      console.log("result[0]: " + result[0] + " "+ result[0].guest.name);
-     var start = "" + result[i].start[2]+ "-" + result[i].start[1] + "-" + result[i].start[0]
-     var eind = "" + result[i].eind[2]+ "-" + result[i].eind[1] + "-" + result[i].eind[0]
+
+     var start = ""
+      if(result[i].start != null) {
+        start += result[i].start[2]+ "-" + result[i].start[1] + "-" + result[i].start[0]
+      }
+     var eind = ""
+      if(result[i].eind != null) {
+      eind += result[i].eind[2]+ "-" + result[i].eind[1] + "-" + result[i].eind[0]
+      }
 
      dataSet.push([result[i].bookingNumber, result[i].guest.name, result[i].guest.address, start, eind, result[i].room.roomNumber, result[i].room.roomType]);
     }
